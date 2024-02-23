ed5f64b8549c80bb4c59e672ad5688694016e2d4
Graziele Fagundes <grazifagundesm@gmail.com>
2024-02-23 00:45:26 -0300

    edit

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,7 +13,6 @@ return new class extends Migration
     {
         Schema::create('usuarios', function (Blueprint $table) {
             $table->id();
-            $table->boolean('administrador')->default(false);
             $table->string('email')->unique();
             $table->string('senha');
             $table->string('nome');
